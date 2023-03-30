2b7c3c2ac0d986754fc3ca816cb23053bbae2553
JKEOSHKE3745 <JKEOSHKE3745@W10-9R51NL3>
2023-03-30 08:50:05 -0500

    Add project files.

---
+++ Cerulli Project 2/Form1.Designer.vb
@@ -0,0 +1,334 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.lblFica = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.lblGross = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.lblFwt = New System.Windows.Forms.Label()
+        Me.lblNet = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.cboAllowances = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.radMarried = New System.Windows.Forms.RadioButton()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.radSingle = New System.Windows.Forms.RadioButton()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.lstRates = New System.Windows.Forms.ListBox()
+        Me.lstHours = New System.Windows.Forms.ListBox()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(246, 79)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(335, 44)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 9
+        Me.PictureBox1.TabStop = False
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.lblFica)
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Controls.Add(Me.lblGross)
+        Me.GroupBox2.Controls.Add(Me.Label7)
+        Me.GroupBox2.Controls.Add(Me.lblFwt)
+        Me.GroupBox2.Controls.Add(Me.lblNet)
+        Me.GroupBox2.Controls.Add(Me.Label8)
+        Me.GroupBox2.Controls.Add(Me.Label9)
+        Me.GroupBox2.Location = New System.Drawing.Point(58, 285)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(529, 88)
+        Me.GroupBox2.TabIndex = 8
+        Me.GroupBox2.TabStop = False
+        '
+        'lblFica
+        '
+        Me.lblFica.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblFica.Location = New System.Drawing.Point(272, 43)
+        Me.lblFica.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lblFica.Name = "lblFica"
+        Me.lblFica.Size = New System.Drawing.Size(112, 31)
+        Me.lblFica.TabIndex = 5
+        Me.lblFica.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(12, 25)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(57, 13)
+        Me.Label6.TabIndex = 0
+        Me.Label6.Text = "Gross pay:"
+        '
+        'lblGross
+        '
+        Me.lblGross.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblGross.Location = New System.Drawing.Point(16, 43)
+        Me.lblGross.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lblGross.Name = "lblGross"
+        Me.lblGross.Size = New System.Drawing.Size(112, 31)
+        Me.lblGross.TabIndex = 1
+        Me.lblGross.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(140, 25)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(34, 13)
+        Me.Label7.TabIndex = 2
+        Me.Label7.Text = "FWT:"
+        '
+        'lblFwt
+        '
+        Me.lblFwt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblFwt.Location = New System.Drawing.Point(144, 43)
+        Me.lblFwt.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lblFwt.Name = "lblFwt"
+        Me.lblFwt.Size = New System.Drawing.Size(112, 31)
+        Me.lblFwt.TabIndex = 3
+        Me.lblFwt.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblNet
+        '
+        Me.lblNet.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblNet.Location = New System.Drawing.Point(400, 43)
+        Me.lblNet.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lblNet.Name = "lblNet"
+        Me.lblNet.Size = New System.Drawing.Size(112, 31)
+        Me.lblNet.TabIndex = 7
+        Me.lblNet.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(268, 25)
+        Me.Label8.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(33, 13)
+        Me.Label8.TabIndex = 4
+        Me.Label8.Text = "FICA:"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(396, 25)
+        Me.Label9.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(47, 13)
+        Me.Label9.TabIndex = 6
+        Me.Label9.Text = "Net pay:"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.txtName)
+        Me.GroupBox1.Controls.Add(Me.cboAllowances)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.radMarried)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.radSingle)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.lstRates)
+        Me.GroupBox1.Controls.Add(Me.lstHours)
+        Me.GroupBox1.Location = New System.Drawing.Point(58, 138)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(684, 126)
+        Me.GroupBox1.TabIndex = 5
+        Me.GroupBox1.TabStop = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(17, 26)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(38, 13)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "&Name:"
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(17, 46)
+        Me.txtName.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(254, 20)
+        Me.txtName.TabIndex = 1
+        '
+        'cboAllowances
+        '
+        Me.cboAllowances.FormattingEnabled = True
+        Me.cboAllowances.Location = New System.Drawing.Point(603, 43)
+        Me.cboAllowances.Name = "cboAllowances"
+        Me.cboAllowances.Size = New System.Drawing.Size(54, 21)
+        Me.cboAllowances.TabIndex = 9
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(401, 26)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(38, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "&Hours:"
+        '
+        'radMarried
+        '
+        Me.radMarried.Location = New System.Drawing.Point(304, 55)
+        Me.radMarried.Margin = New System.Windows.Forms.Padding(2)
+        Me.radMarried.Name = "radMarried"
+        Me.radMarried.Size = New System.Drawing.Size(84, 19)
+        Me.radMarried.TabIndex = 3
+        Me.radMarried.Text = "&Married"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(490, 26)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(33, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "&Rate:"
+        '
+        'radSingle
+        '
+        Me.radSingle.Checked = True
+        Me.radSingle.Location = New System.Drawing.Point(304, 26)
+        Me.radSingle.Margin = New System.Windows.Forms.Padding(2)
+        Me.radSingle.Name = "radSingle"
+        Me.radSingle.Size = New System.Drawing.Size(84, 19)
+        Me.radSingle.TabIndex = 2
+        Me.radSingle.TabStop = True
+        Me.radSingle.Text = "&Single"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(599, 26)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(64, 13)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "&Allowances:"
+        '
+        'lstRates
+        '
+        Me.lstRates.FormattingEnabled = True
+        Me.lstRates.Location = New System.Drawing.Point(492, 43)
+        Me.lstRates.Margin = New System.Windows.Forms.Padding(2)
+        Me.lstRates.Name = "lstRates"
+        Me.lstRates.Size = New System.Drawing.Size(78, 56)
+        Me.lstRates.TabIndex = 7
+        '
+        'lstHours
+        '
+        Me.lstHours.FormattingEnabled = True
+        Me.lstHours.Location = New System.Drawing.Point(403, 43)
+        Me.lstHours.Margin = New System.Windows.Forms.Padding(2)
+        Me.lstHours.Name = "lstHours"
+        Me.lstHours.Size = New System.Drawing.Size(61, 56)
+        Me.lstHours.TabIndex = 5
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(672, 328)
+        Me.btnExit.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(70, 31)
+        Me.btnExit.TabIndex = 7
+        Me.btnExit.Text = "E&xit"
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Location = New System.Drawing.Point(592, 328)
+        Me.btnCalc.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(76, 31)
+        Me.btnCalc.TabIndex = 6
+        Me.btnCalc.Text = "&Calculate"
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(750, 377)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Name = "frmMain"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents lblFica As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents lblGross As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents lblFwt As Label
+    Friend WithEvents lblNet As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents cboAllowances As ComboBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents radMarried As RadioButton
+    Friend WithEvents Label4 As Label
+    Friend WithEvents radSingle As RadioButton
+    Friend WithEvents Label5 As Label
+    Friend WithEvents lstRates As ListBox
+    Friend WithEvents lstHours As ListBox
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnCalc As Button
+End Class
